9fc62355a90b94b5bc9d8facfc08c6eaa054358d
umarsahab <121605324+umarsahab@users.noreply.github.com>
2023-05-06 14:25:35 +0500

    submit

---
+++ app.js
@@ -0,0 +1,37 @@
+// Question 1
+
+var userName;
+// Question 2
+var myName = "Muhammad Umar";
+// Question 3
+var message = "Hello World"
+alert(message)
+// Question 4
+var studentName = prompt("Inter your Name")
+var studentFatherName = prompt("Inter your Father Name")
+var studentAge = prompt("Inter your Age")
+var studentClass = prompt("Inter your Class")
+alert(studentName)
+alert(studentFatherName)
+alert(studentAge)
+alert(studentClass)
+
+// Question 5
+var pizza;
+pizza = alert("PIZZA" + '\n' + "PIZZ" + '\n' + "PIZ" + '\n' + "PI" + '\n' + "P")
+
+// Question 6
+var email = prompt('Inter your email address')
+alert("My Email Address is " + email)
+
+// Question 7
+var book = "A Smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book)
+
+// Question 8
+document.write("Yah! I can write HTML content through JavaScript")
+
+// Question 9
+var emoji = "-------------*******--------------"
+alert(emoji)
+document.write("-------------*******--------------")
